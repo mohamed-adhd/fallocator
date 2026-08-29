b1d045edcd87f2c8c8ff6c4557db2ed7b136a8cb
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-29 21:38:03 +0000

    fixed the loop syntax and adding the split logic

--- fortran-allocator/src/heap.f90
+++ fortran-allocator/src/heap.f90
@@ -6,10 +6,20 @@ module heap
   end type heap
   contains
     function func(i) result(ptr)
-      type(block), pointer :: temp => start
-      do while (temp%next/=null)
-      temp=temp%next
+      type(heap), intent(inout) :: h
+      integer, intent(in) :: i
+      type(block), pointer :: ptr
+      emp => h%start
+      do while (associated(temp%next))
+        if
+      temp=>temp%next
       end do
+
     end function
+  function swap (ptr,i) result (ptr)
+    type(block), pointer :: part1 => null()
+    type(block), pointer :: part2 => null()
+
+  end function
 
 end module heap
