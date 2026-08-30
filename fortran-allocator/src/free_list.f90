09411d4c2a6712f75f1cc4bdb3fa8dd945cf81df
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-30 18:56:57 +0000

    since heap used the previous block struct , nuked it and starting replacement with subroutines and correct usage of block struct

--- fortran-allocator/src/free_list.f90
+++ fortran-allocator/src/free_list.f90
@@ -1,10 +1,7 @@
 module freelist
-
     use blocks
     implicit none
-
     type :: freelist
         type(block), pointer :: head => null()
     end type free_list
-
 end module freelist
--- fortran-allocator/src/heap.f90
+++ fortran-allocator/src/heap.f90
@@ -1,38 +1,10 @@
 module heap
-  implicit none
   use blocks
+  use, intrinsic :: iso_c_binding
+  implicit none
     type::heap
-      type(block), pointer :: start => null()
+      type(c_ptr) :: start = c_null_ptr
   end type heap
   contains
-  function swap (ptr,i) result (ptr)
-    type(block), pointer :: part1 => null()
-    type(block), pointer :: part2 => null()
-    part1%size=i
-    part1%state=FALSE
-    part1%prev=>ptr%prev
-  end function
-    function func(i) result(ptr)
-      logical :: done=FALSE
-      type(heap), intent(inout) :: h
-      integer, intent(in) :: i
-      type(block), pointer :: ptr
-      emp => h%start
-      do while (associated(temp%next))
-        if(temp%size==i) then
-          temp%state=FALSE
-          done=TRUE
-        else if (temp%size>i) then
-          call swap(temp,i)
-          done=TRUE
-        end if
-      temp=>temp%next
-      end do
-      if (done==FALSE) then
-        !i will call the platformer here
-      end if
-
-    end function
-
-
+  
 end module heap
