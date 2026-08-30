61161a500af36aeb0bdaae3243c8e100003d5dec
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-30 18:42:08 +0000

    major refractor of the block structure

--- fortran-allocator/src/block.f90
+++ fortran-allocator/src/block.f90
@@ -1,11 +1,11 @@
 module blocks
+    use, intrinsic :: iso_c_binding
     implicit none
-    type::block
-        integer::size
-        !nvmd..integer::state!1 for occupied and 0 for free dave
-        logical :: state
-        type(block), pointer :: next => null()
-        type(block), pointer :: prev => null()
-
+    type, bind(C) :: block
+        integer(c_size_t) :: size
+        logical(c_bool) :: state
+        type(c_ptr) :: next
+        type(c_ptr) :: prev
     end type block
 end module blocks
+!nvmd..integer::state!1 for occupied and 0 for free dave
--- fortran-allocator/src/heap.f90
+++ fortran-allocator/src/heap.f90
@@ -13,20 +13,24 @@ module heap
     part1%prev=>ptr%prev
   end function
     function func(i) result(ptr)
-      logical :: done
+      logical :: done=FALSE
       type(heap), intent(inout) :: h
       integer, intent(in) :: i
       type(block), pointer :: ptr
       emp => h%start
       do while (associated(temp%next))
         if(temp%size==i) then
-          temp%state=False
+          temp%state=FALSE
+          done=TRUE
         else if (temp%size>i) then
           call swap(temp,i)
-
+          done=TRUE
         end if
       temp=>temp%next
       end do
+      if (done==FALSE) then
+        !i will call the platformer here
+      end if
 
     end function
 
