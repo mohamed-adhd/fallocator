8cd77e8d08626f4c4144ef49a9bccb45b5d25a3c
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-30 19:22:51 +0000

    alright finished the exact match/bigger stake cases , building the split logic

--- fortran-allocator/src/heap.f90
+++ fortran-allocator/src/heap.f90
@@ -6,11 +6,12 @@ module heap
       type(c_ptr) :: start = c_null_ptr
   end type heap
   contains
-  subroutine swap
+  subroutine split
 
-  end subroutine swap
+  end subroutine split
   function find(i,h) result(ptr)
     type(c_ptr) :: current_addr
+    logical::done
     type(block), pointer :: current
     type(heap), intent(inout) :: h
     integer(c_size_t), intent(in) :: i
@@ -19,10 +20,16 @@ module heap
     current_addr = h%start
     do while (c_associated(current_addr))
       call c_f_pointer(current_addr, current)
-
-
+      if(current%size==i .and. current%state==FALSE)then
+        current%state=TRUE! false for free and true for busy
+        ptr = current_addr
+        return
+      else if (current%size>=i) then
+        ptr = current_addr
+        call split(current, i)
+        return
+      end if
       current_addr = current%next
-
     end do
   end function find
 end module heap
