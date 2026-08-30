6dd4943d4acff3655aec8dbb89e69c5a38ac9a1f
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-30 19:10:17 +0000

    built the base of the find function , adding the comparaison then moving on to the block split

--- fortran-allocator/src/heap.f90
+++ fortran-allocator/src/heap.f90
@@ -6,5 +6,23 @@ module heap
       type(c_ptr) :: start = c_null_ptr
   end type heap
   contains
-  
+  subroutine swap
+
+  end subroutine swap
+  function find(i,h) result(ptr)
+    type(c_ptr) :: current_addr
+    type(block), pointer :: current
+    type(heap), intent(inout) :: h
+    integer(c_size_t), intent(in) :: i
+    type(c_ptr) :: ptr
+    ptr = c_null_ptr
+    current_addr = h%start
+    do while (c_associated(current_addr))
+      call c_f_pointer(current_addr, current)
+
+
+      current_addr = current%next
+
+    end do
+  end function find
 end module heap
