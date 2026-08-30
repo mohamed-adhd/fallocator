48704faa80d60640056ef470a8a3614611fd54d8
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-30 18:27:42 +0000

    added the comparaison logic for the blocks

--- fortran-allocator/src/heap.f90
+++ fortran-allocator/src/heap.f90
@@ -5,17 +5,6 @@ module heap
       type(block), pointer :: start => null()
   end type heap
   contains
-    function func(i) result(ptr)
-      type(heap), intent(inout) :: h
-      integer, intent(in) :: i
-      type(block), pointer :: ptr
-      emp => h%start
-      do while (associated(temp%next))
-        if
-      temp=>temp%next
-      end do
-
-    end function
   function swap (ptr,i) result (ptr)
     type(block), pointer :: part1 => null()
     type(block), pointer :: part2 => null()
@@ -23,5 +12,23 @@ module heap
     part1%state=FALSE
     part1%prev=>ptr%prev
   end function
+    function func(i) result(ptr)
+      logical :: done
+      type(heap), intent(inout) :: h
+      integer, intent(in) :: i
+      type(block), pointer :: ptr
+      emp => h%start
+      do while (associated(temp%next))
+        if(temp%size==i) then
+          temp%state=False
+        else if (temp%size>i) then
+          call swap(temp,i)
+
+        end if
+      temp=>temp%next
+      end do
+
+    end function
+
 
 end module heap
