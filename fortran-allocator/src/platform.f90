f89effcf5ae9570ed9925814badab7980ef91383
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-31 20:50:22 +0000

    fixed all compiling errors ,rest is just for another day ..

--- fortran-allocator/src/platform.f90
+++ fortran-allocator/src/platform.f90
@@ -1,11 +1,21 @@
 module platform
     use, intrinsic :: iso_c_binding
     implicit none
-    interface
-        function gimme_ram(size) bind(C, name="gimme_ram") result(ptr)
-            import :: c_size_t, c_ptr
-            integer(c_size_t), value :: size
-            type(c_ptr) :: ptr
-            end function gimme_ram
-    end interface
+
+    integer(c_size_t), parameter :: ARENA_SIZE = 100_c_size_t * 1024_c_size_t * 1024_c_size_t  ! 100 MB
+    integer(c_int8_t), target, save :: arena(ARENA_SIZE)
+    integer(c_size_t), save :: bump_offset = 0_c_size_t
+contains
+    function gimme_ram(size) result(ptr)
+        integer(c_size_t), intent(in) :: size
+        type(c_ptr) :: ptr
+        integer(c_intptr_t) :: base_addr
+        if (bump_offset + size > ARENA_SIZE) then
+            ptr = c_null_ptr
+            return
+        end if
+        base_addr = transfer(c_loc(arena(bump_offset + 1_c_size_t)), base_addr)
+        ptr = transfer(base_addr, ptr)
+        bump_offset = bump_offset + size
+    end function gimme_ram
 end module platform
