11d0aefb19e2d87662e3550885291437d086c012
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-31 11:51:37 +0000

    finisihed the stat display and building the locate input

--- fortran-allocator/src/allocator.f90
+++ fortran-allocator/src/allocator.f90
@@ -1,6 +1,12 @@
 program shi
     implicit none
-    character(len=142), parameter :: banner(8) = [ character(len=142) :: &
+    character(len=*), parameter :: status = "stats   : "
+    character(len=*), parameter :: fm = "free memory = 2465 Mb"
+    character(len=*), parameter :: tb = "total blocks = 5"
+    character(len=*), parameter :: allocate= "(enter how much mb you want to allocate) : "
+    integer:: how_much_for_a_banana
+    integer :: ios
+    character(len=142), parameter :: banner(10) = [ character(len=142) :: &
             '                                    _______    ___       __       __        ______     ______     ___   .___________.  ______   .______      ', &
                     '                                    |   ____|  /   \     |  |     |  |      /  __  \   /      |   /   \  |           | /  __  \  |   _  \     ', &
                     '                                    |  |__    /  ^  \    |  |     |  |     |  |  |  | |  ,----"  /  ^  \ `---|  |----`|  |  |  | |  |_)  |    ', &
@@ -8,11 +14,24 @@ program shi
                     '                                    |  |    /  _____  \  |  `----.|  `----.|  `--"  | |  `----./  _____  \   |  |     |  `--"  | |  |\  \----.', &
                     '                                    |__|   /__/     \__\ |_______||_______| \______/   \______/__/     \__\  |__|      \______/  | _| `._____|',  &
                     '                                                                                                                                              ', &
-                    '                                                                                by mohamed-adhd :D                                            ' &
+                    '                                                                                by mohamed-adhd :D                                            ', &
+                    '                                                                                                                                              ', &
+                    '                                                                                                                                              ' &
+
             ]
     integer :: i
     call execute_command_line('clear')
-    do i = 1, 8
+    do i = 1, 10
         print *,banner(i)
     end do
+    print *,status
+    print *,fm
+    print *,tb
+    print *,allocate
+    do
+
+
+
+
+
 end program shi
